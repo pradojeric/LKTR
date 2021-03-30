23298209e182c88ec42a5237612f5df8a848119e
pradojeric <jericprado0719@gmail.com>
2021-03-30 11:26:10 +0800

    new

--- app/Http/Controllers/EventLeaderboardController.php
+++ app/Http/Controllers/EventLeaderboardController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\EventLeaderboard;
 use App\GameEvent;
+use App\EventLeaderboard;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EventLeaderboardController extends Controller
 {
@@ -25,12 +26,22 @@ class EventLeaderboardController extends Controller
     public function uploadScore(Request $request, $code)
     {
         if($code == "arzA-Game+20"){
+
+            $validator = Validator::make($request->all(),[
+                'uId' => 'required',
+                'game_event_id' => 'required',
+            ]);
+
+            if($validator->fails()){
+                return response()->json(['error' => $validator->errors()->all()]);
+            }
+
             EventLeaderboard::updateOrCreate([
                 'event_users_id' => $request->uId,
-                'game_event_id' => $request->course_id,
+                'game_event_id' => $request->game_event_id,
             ],[
                 'event_users_id' => $request->uId,
-                'game_event_id' => $request->course_id,
+                'game_event_id' => $request->game_event_id,
                 'score' => $request->score,
             ]);
 
